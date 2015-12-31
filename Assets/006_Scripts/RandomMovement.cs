0c12c92f8b9f2139e929e6f6fd1977bbca72d4aa
Tan <m.tan2007@gmail.com>
2015-12-31 17:47:26 +0700

    :D

--- Assets/006_Scripts/RandomMovement.cs
+++ Assets/006_Scripts/RandomMovement.cs
@@ -1,21 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
 public class RandomMovement : MonoBehaviour {
+	[Range(1.0f, 20.0f)]
+	public float speed = 5.0f;
 
-	Vector3 t;
-	// Use this for initialization
-	void Start () {
-		t = transform.localPosition;
-	}
-	
+	[Range(1.0f, 50.0f)]
+	public float displacement = 10.0f;
+
+	bool isCouRunning = false;
+
 	// Update is called once per frame
 	void Update () {
-		
+		if(!isCouRunning){
+			MoveMeRandomly ();
+		}
 	}
 
 	void MoveMeRandomly(){
+		Vector3 vel;
 
+		vel = Random.insideUnitSphere * displacement;
+		vel.z = 0.0f;
+		vel.x = 0.0f;
+
+		StartCoroutine (MoveTo(vel));
+	}
+
+	IEnumerator MoveTo(Vector3 destination){
+		isCouRunning = true;
+		do{
+			float step = speed * Time.deltaTime;
+			transform.localPosition = Vector3.MoveTowards(transform.localPosition, destination, step);
+			yield return null;
+		}while(Vector3.Distance(transform.localPosition, destination) > 0.001f);
+		isCouRunning = false;
 	}
 
 }
--- Assets/006_Scripts/ToggleStateChange.cs
+++ Assets/006_Scripts/ToggleStateChange.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class ToggleStateChange : MonoBehaviour {
+	public float scale = 1.4f;
+	public AutoRotate autoRotate;
+
 	void Start(){
 		gameObject.GetComponent<Toggle> ().onValueChanged.AddListener (OnValueChange);
 	}
@@ -10,8 +13,14 @@ public class ToggleStateChange : MonoBehaviour {
 	public void OnValueChange(bool isOn){
 		if (!isOn) {
 			iTween.ScaleTo (gameObject, iTween.Hash("scale", Vector3.one, "easeType", "easeOutBack", "time", 0.25f));
+			if(autoRotate != null){
+				autoRotate.enabled = false;
+			}
 		} else {
-			iTween.ScaleTo (gameObject, iTween.Hash("scale", new Vector3(1.5f, 1.5f, 1.5f), "easeType", "easeOutBack", "time", 0.25f));
+			iTween.ScaleTo (gameObject, iTween.Hash("scale", new Vector3(scale, scale, scale), "easeType", "easeOutBack", "time", 0.25f));
+			if(autoRotate != null){
+				autoRotate.enabled = true;
+			}
 		}
 	}
 }
